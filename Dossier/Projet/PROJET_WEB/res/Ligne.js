ddf51a720e02fcba01f7b2c234f24a1d49f8fc8d
Yann Adjumany <69991917+A-yann-axel-V@users.noreply.github.com>
2023-10-05 17:47:54 +0000

    news

---
+++ Dossier/Projet/PROJET_WEB/res/Ligne.js
@@ -0,0 +1,6 @@
+// Classe du projet
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJ4X3Bjc29mdF9ub21fbG9naXF1ZSI6IkxpZ25lIiwieF9wY3NvZnRfdHlwZV9sb2dpcXVlIjoiNCIsInhfcGNzb2Z0X2lkX2Vuc2VtYmxlIjoiNDk3OTkyNjg3MjE4NDg5NDEwOSIsIm1hcHBpbmdzIjoiQSJ9
+function LIGNE_LOD(event){var __VARS0=new NSPCS.NSValues.CVariablesLocales(0,1);try{clWDUtil.Try();{void 0;}}catch(_E){clWDUtil.xbCatchThrow(_E,event);return;}finally{clWDUtil&&clWDUtil.oFinally();}}LIGNE_LOD[NSPCS.ms_oSymbolDepuisWLEnV2]=true;
+function LIGNE_Constructeur(){var __VARS0=new NSPCS.NSValues.CVariablesLocales(0,0);try{clWDUtil.Try();__VARS0.xVerifieParametres(arguments.length);{void 0;}}catch(_E){clWDUtil.xbCatchThrow(_E);return;}finally{clWDUtil&&clWDUtil.oFinally();}}LIGNE_Constructeur[NSPCS.ms_oSymbolDepuisWLEnV2]=true;
+function LIGNE_Destructeur(){var __VARS0=new NSPCS.NSValues.CVariablesLocales(0,0);}LIGNE_Destructeur[NSPCS.ms_oSymbolDepuisWLEnV2]=true;
+var Ligne=new NSPCS.NSTypes.CDescriptionClasse({},{"Ref":function(){return NSPCS.NSTypes.iGetDescription(16);},"Libellé":function(){return NSPCS.NSTypes.iGetDescription(16);},"Qte":function(){return NSPCS.NSTypes.iGetDescription(8);}},LIGNE_Constructeur,{},{},"Ligne");
